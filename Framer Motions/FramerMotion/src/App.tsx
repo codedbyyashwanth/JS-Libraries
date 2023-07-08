b781a38e3a8368b1eb01ea30890a8aaae05cc8cf
codedbyyashwanth <yashwanthpcmc5@gmail.com>
2023-07-08 23:41:11 +0530

    Finished till 8 Videos

--- Framer Motions/FramerMotion/src/App.tsx
+++ Framer Motions/FramerMotion/src/App.tsx
@@ -1,35 +1,50 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { FC, useState } from "react";
+import { motion } from "framer-motion";
 
-function App() {
-  const [count, setCount] = useState(0)
+const App:FC = () => {
+      const [visible, setVisible] = useState<boolean>(false);
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+      const textVariant = {
+            hidden: {
+                  y: "-20px", 
+                  opacity: 0
+            },
+            animate: {
+                  y: 0, 
+                  opacity: 1,
+                  transition: {
+                        type: "tween",
+                        when: "beforeChildren"
+                  }
+            }
+      }
+
+      const childVariant = {
+            hidden: {
+                  opacity: 0
+            },
+            animate: {
+                  opacity: 1,
+
+                  transition: {
+                        duration: 1
+                  }
+            }
+      }
+
+      return <div className=" bg-blue-500 w-full h-screen flex justify-center items-center flex-col gap-y-2">
+            <motion.div 
+                  className=" w-48 h-48 bg-white rounded-3xl shadow-xl">
+            </motion.div>
+
+            <button className="px-5 py-2 bg-teal-400 rounded-md" onClick={() => setVisible(!visible)}>Show Text</button>
+            {
+                  visible && <motion.h2 variants={textVariant} initial="hidden"  animate="animate" className=" text-lg font-medium text-center">
+                        Hello, There! <br />
+                              <motion.span className=" text-center" variants={childVariant} >Thank You!</motion.span>
+                        </motion.h2>
+            }
+      </div>;
 }
 
 export default App
